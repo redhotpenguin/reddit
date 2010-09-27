543e54cc6a32f4a1a7c306b844112a3d12fa4fec
Fred Moyer <fred@redhotpenguin.com>
2010-09-27 11:28:27 -0700

    More stuff.

--- scripts/rising.sh
+++ scripts/rising.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-cd ~/reddit/r2
-/usr/local/bin/saferun /tmp/rising.pid /usr/local/bin/paster run run.ini r2/lib/rising.py -c "set_rising()"
+cd /home/reddit/reddit/r2
+/usr/local/bin/paster run run.ini r2/lib/rising.py -c "set_rising()"
--- scripts/update_promos.sh
+++ scripts/update_promos.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-cd ~/reddit/r2
+cd /home/reddit/reddit/r2
 /usr/local/bin/paster run run.ini -c "from r2.lib import promote; promote.Run()"
